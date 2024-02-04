265aca308517842e93877038bdac5d73325e8158
Ali <alicode6.cs@gmail.com>
2024-02-04 16:25:22 +0500

    Counter problem Solved

--- 02countervite/src/App.jsx
+++ 02countervite/src/App.jsx
@@ -9,7 +9,17 @@ function App() {
   
   const incrementValue = ()=>{
     if(counter >= 0 && counter < 20){
+      /*
       setCounter(counter+1);
+      setCounter(counter+1);
+      setCounter(counter+1);
+      */
+      //the above lines wont update the counter to +3 because the final value is just the same as the counter neverchanges through out the lines, the useState send data in a single junk and one click will send the data which will be counter+1.
+      //to make these lines works, we can do it as,
+      setCounter(prevCounter => prevCounter+1); // setCounter can accept callback method and hence everyline value is stored so the final value is passed
+      setCounter(prevCounter => prevCounter+1);
+      setCounter(prevCounter => prevCounter+1);
+      // now the counter will go with +3 on every Click
     }
   }
   const decrimentValue = ()=> {
