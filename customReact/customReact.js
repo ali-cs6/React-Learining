0fbe52ff5328a4230c5b34f050013ab225d026ac
Ali <alicode6.cs@gmail.com>
2024-05-15 18:16:29 +0500

    comments added

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,4 +1,4 @@
-function customRender(reactElement, container){ //this function will do the whole function, first parameter is what we wil inject, 2nd element where we will inject
+function customRender(reactElement, container){ //this function will do the whole function, first parameter is what we will inject, 2nd parameter is where we will inject our element
     //this is not an optimized code lol
     /*
     const domElement = document.createElement(reactElement.type);
@@ -11,17 +11,17 @@ function customRender(reactElement, container){ //this function will do the whol
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
     for (const prop in reactElement.props){
-        if(prop === 'children') continue;
+        if(prop === 'children') continue; //if incase there is a children key inside props and is not actually the property directly, so will ignore it
         domElement.setAttribute(prop, reactElement.props[prop]);
     }
     container.appendChild(domElement)
 }
 
 const reactElement = { //inject. this is how jsx form is converted, this is how react undertsand the code
-    type: 'a', //its type
+    type: 'a', //what type of tag
     props: { // attributes for above html element
         href: 'https://google.com',
-        target: '__blank'
+        target: '__blank' //this means that click on anchor tag will open/lead to a new tab
     },
     children: 'Click me to visit google' //children to the added element (content of reactElement)
 }
